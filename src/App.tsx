2e16e8fd94d01f6cc0fcf9267ec5d9792f1584c7
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2021-07-02 04:07:04 +0200

    phase 1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const App = () => (
+  <>
+    <h1>My Company</h1>
+    <p>Esse consequat do nostrud non amet mollit sunt commodo sint eu nisi cillum nostrud. In non fugiat laborum est esse commodo voluptate sunt non aliquip. Ipsum ex sint ipsum tempor ipsum ut velit. Laboris velit consequat dolor aliquip.</p>
+
+    <p>Anim fugiat ullamco amet ullamco laboris aliquip ut in ut esse. Cupidatat amet nulla ut deserunt duis ipsum aliqua cillum veniam ex elit aliqua esse. Laborum dolore ex cupidatat culpa incididunt Lorem dolor. Ut ipsum enim magna et ex id officia labore eu nulla. Esse fugiat in consequat magna tempor. Exercitation occaecat cillum occaecat laboris eu irure ad commodo cillum dolore id anim duis elit.</p>
+
+    <p>Officia et qui tempor aliquip non quis. Nisi tempor exercitation nostrud veniam non sint exercitation laborum sint et. Ipsum enim mollit ullamco anim adipisicing esse do. Esse exercitation ea reprehenderit duis deserunt sit.</p>
+
+    <p>Consequat minim veniam minim sint commodo incididunt id reprehenderit aliquip do ut duis sint dolor. Dolor ea aliqua mollit eiusmod reprehenderit. Amet culpa ex aute sint aute velit ullamco.</p>
+
+    <p>Id dolor minim veniam ullamco occaecat est. Sunt minim et ex commodo nostrud pariatur sint adipisicing. Consequat aliqua adipisicing velit sint sit in. Occaecat exercitation laborum et nisi adipisicing labore et irure ea. Nisi officia aliquip cillum in duis laboris dolore in sunt culpa sunt. Incididunt laboris culpa ut consectetur reprehenderit officia. Tempor do ullamco magna cupidatat esse nostrud aliquip anim velit cillum amet tempor.</p>
+  </>
+);
+
+export default App;
